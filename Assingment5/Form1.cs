d1239756ce669ccaeee9fe04b5e02d7e01cb9615
JunnatulFerdouseProme <55207718+JunnatulFerdouseProme@users.noreply.github.com>
2019-09-21 22:19:07 +0600

    final edit

--- Assingment5/Form1.cs
+++ Assingment5/Form1.cs
@@ -12,170 +12,206 @@ namespace Assingment5
 {
     public partial class studentForm : Form
     {
-        ErrorProvider erp = new ErrorProvider();
+       
 
-
-        List<string> ids = new List<string>();
-        List<string> names = new List<string>();
-        List<string> mobiles = new List<string>();
-        List<string> ages = new List<string>();
-        List<string> addres = new List<string>();
-        List<string> gpas = new List<string>();
-
-        string message = " ";
-        int index = 0;
-        public studentForm()
+List<int> studentID = new List<int> { };
+        List<string> studentName = new List<string> { };
+        List<string> mobile = new List<string> { };
+        List<int> age = new List<int> { };
+        List<string> address = new List<string> { };
+        List<double> gpa = new List<double> { };
+        public Student()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void AddButton_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void gpaLabel_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void addButton_Click(object sender, EventArgs e)
-        {
-            erp.Clear();
-
-            if (idTextBox.Text.Equals(" ") || idTextBox.Text.Length != 4)
+            if (String.IsNullOrEmpty(idTextBox.Text))
             {
-                index++;
-                erp.SetError(idTextBox, "Enter your Id in 4 character");
-                return;
-            }
-            else if (checkDuplicateID()) ;
-            else if (nameTextBox.Text.Equals(" ") || nameTextBox.Text.Length > 30)
-            {
-                index++;
-                erp.SetError(nameTextBox, "Enter your Name less then 30 character");
+                MessageBox.Show("Please enter your id");
                 return;
             }
 
-            else if (mobileTextBox.Text.Equals(" ") || mobileTextBox.Text.Length != 11)
+            if (idTextBox.Text.Length != 4)
             {
-                index++;
-                erp.SetError(phoneTextBox, "Enter your phone number in 11 character");
-                return;
-            }
-            else if (checkDuplicateMobile()) ;
-
-            else if (ageTextBox.Text.Equals(" ") || ageTextBox.Text.Length > 50)
-            {
-                index++;
-                erp.SetError(ageTextBox, "Enter your age ");
+                MessageBox.Show("Id must be in 4 character!");
                 return;
             }
 
-            else if (addressTextBox.Text.Equals(" "))
+            //Unique
+            if (studentID.Contains(Convert.ToInt32(idTextBox.Text)))
             {
-                index++;
-                erp.SetError(addressTextBox, "Write your address ");
+                MessageBox.Show("Already exits");
                 return;
             }
 
-            else if (gpaTextBox.Text.Equals(" ") || (float.Parse(gpaTextBox.Text) < 1 || float.Parse(gpaTextBox.Text) > 4))
+            if (String.IsNullOrEmpty(nameTextBox.Text) || nameTextBox.Text.Length >= 30)
             {
-                index++;
-                erp.SetError(gpaTextBox, "GPA point must be in 1-4 ");
+                MessageBox.Show("Please enter Name and Name must be in 30 character!");
                 return;
             }
 
-            ids.Add(idTextBox.Text);
-            names.Add(nameTextBox.Text);
-            mobiles.Add(mobileTextBox.Text);
-            ages.Add(ageTextBox.Text);
-            addres.Add(addressTextBox.Text);
-            gpas.Add(gpaTextBox.Text);
-
-            richTextBox.Text = "ID : " + idTextBox.Text + "\n" + "Name : " + nameTextBox.Text + "\n" + "Mobile : " + mobileTextBox.Text + "\n" + "Age : " + ageTextBox.Text + "\n" + "Address : " + addressTextBox.Text + "\n" + "GPA : " + gpaTextBox.Text + "\n";
-            MessageBox.Show("Student is admitted");
-            Clear();
-
-        }
-
-        private void showButton_Click(object sender, EventArgs e)
-        {
-            String minName = " ", maxName = " ";
-            float min = 10, max = 0, avg = 0, total = 0, gpatext;
-
-
-            for (int j = 0; j < ids.Count(); j++)
+            if (String.IsNullOrEmpty(mobileTextBox.Text) || mobileTextBox.Text.Length != 11)
             {
-                gpatext = float.Parse(gpas[j]);
-                total += gpatext;
 
-                if (min > gpatext)
-                {
-                    min = gpatext;
-                    minName = names[j];
-                }
-
-                if (max < gpatext)
-                {
-                    max = gpatext;
-                    maxName = names[j];
-
-                }
-
-                message += "ID : " + ids[j] + "\n" + "Name : " + names[j] + "\n" + "Mobile : " + mobiles[j] + "\n" + "Age : " + ages[j] + "\n" + "Address : " + addres[j] + "\n" + "GPA : " + gpas[j] + "\n" + "\n";
-
-                maxTextBox.Text = max.ToString();
-                nameMaxTextBox.Text = maxName;
-                mimTextBox.Text = min.ToString();
-                mimNameTextBox.Text = minName;
-                totalTextBox.Text = total.ToString();
-                averegeTextBox.Text = (total / ids.Count()).ToString();
+                MessageBox.Show("Please enter Mobile No and Mobile No must be in 11 character!");
+                return;
             }
-            richTextBox.Text = message;
 
-        }
+            if (String.IsNullOrEmpty(gpaTextBox.Text))
+            {
+                MessageBox.Show("Please enter valid CGPA!");
+                return;
+            }
+            if (Convert.ToDouble(gpaTextBox.Text) < 0 || Convert.ToDouble(gpaTextBox.Text) > 5)
+            {
+                MessageBox.Show("Please enter valid CGPA");
+                return;
+            }
+            else
+            {
+                AddStudent(Convert.ToInt32(idTextBox.Text), nameTextBox.Text, mobileTextBox.Text,
+                Convert.ToInt32(ageTextBox.Text), addressTextBox.Text, Convert.ToDouble(gpaTextBox.Text));
+            }
 
-        private void Clear()
-        {
-            nameTextBox.Text = "";
+
+            string output = "";
+            for (int i = 0; i < studentID.Count(); i++)
+            {
+                output = "Student ID: " + studentID[i] + "\n" + "Student Name: " + studentName[i] + "\n"
+                          + "Student Mobile: " + mobile[i] + "\n" + "Student Age: " + age[i] + "\n" + "Student Address: " + address[i] + "\n" + "GPA: " + gpa[i] + "\n\n";
+            }
+            richTextBox.Text = output;
+
             idTextBox.Text = "";
+            nameTextBox.Text = "";
             mobileTextBox.Text = "";
             ageTextBox.Text = "";
             addressTextBox.Text = "";
             gpaTextBox.Text = "";
-            maxTextBox.Text = "";
-            mimTextBox.Text = "";
-            nameMaxTextBox.Text = "";
-            mimNameTextBox.Text = "";
-            averegeTextBox.Text = "";
-            totalTextBox.Text = "";
+        }
+
+        public void AddStudent(int id, string Name, string Mobile, int Age, string Address, double Gpa)
+        {
+            studentID.Add(id);
+            studentName.Add(Name);
+            mobile.Add(Mobile);
+            age.Add(Age);
+            address.Add(Address);
+            gpa.Add(Gpa);
 
         }
 
-    }
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (idRadioButton.Checked == true)
+            {
+                if (studentID.Contains(Convert.ToInt32(idTextBox.Text)))
+                {
+                    int index = studentID.IndexOf(Convert.ToInt32(idTextBox.Text));
+                    MessageBox.Show("ID: " + studentID[index] + "\n" + "Name: " + studentName[index] + "\n" +
+                "Mobile No: " + mobile[index] + "\n" + "Age: " + age[index] + "\n" + "Address: "
+                + address[index] + "\n" + "GPA" + gpa[index] + "\n");
 
-    
-    
+                    richTextBox.Text = ("ID: " + studentID[index] + "\n" + "Name: " + studentName[index] + "\n" +
+                    "Mobile No: " + mobile[index] + "\n" + "Age: " + age[index] + "\n" + "Address: "
+                    + address[index] + "\n" + "GPA" + gpa[index] + "\n");
+
+                }
+            }
+
+            else if (nameRadioButton.Checked == true)
+            {
+                if (studentName.Contains(nameTextBox.Text))
+                {
+                    int index = studentName.IndexOf(nameTextBox.Text);
+                    MessageBox.Show("ID: " + studentID[index] + "\n" + "Name: " + studentName[index] + "\n" +
+                "Mobile No: " + mobile[index] + "\n" + "Age: " + age[index] + "\n" + "Address: "
+                + address[index] + "\n" + "GPA" + gpa[index] + "\n");
+
+                    richTextBox.Text = ("ID: " + studentID[index] + "\n" + "Name: " + studentName[index] + "\n" +
+                    "Mobile No: " + mobile[index] + "\n" + "Age: " + age[index] + "\n" + "Address: "
+                    + address[index] + "\n" + "GPA" + gpa[index] + "\n");
+
+                }
+
+
+            }
+
+
+            else if (mobileRadioButton.Checked == true)
+            {
+                if (mobile.Contains(mobileTextBox.Text))
+                {
+                    int index = mobile.IndexOf(mobileTextBox.Text);
+                    MessageBox.Show("ID: " + studentID[index] + "\n" + "Name: " + studentName[index] + "\n" +
+                "Mobile No: " + mobile[index] + "\n" + "Age: " + age[index] + "\n" + "Address: "
+                + address[index] + "\n" + "GPA" + gpa[index] + "\n");
+
+                    richTextBox.Text = ("ID: " + studentID[index] + "\n" + "Name: " + studentName[index] + "\n" +
+                    "Mobile No: " + mobile[index] + "\n" + "Age: " + age[index] + "\n" + "Address: "
+                    + address[index] + "\n" + "GPA" + gpa[index] + "\n");
+
+
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Data not found");
+            }
+            ClearList();
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            showStudent();
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            mobileTextBox.Text = "";
+            ageTextBox.Text = "";
+            addressTextBox.Text = "";
+            gpaTextBox.Text = "";
+        }
+
+        public void showStudent()
+        {
+            string output = "";
+            for (int i = 0; i < studentID.Count(); i++)
+            {
+                output += "Student ID: " + studentID[i] + "\n" + "Student Name: " + studentName[i] + "\n"
+                          + "Student Mobile: " + mobile[i] + "\n" + "Student Age: " + age[i] + "\n" + "Student Address: " + address[i] + "\n" + "GPA: " + gpa[i] + "\n\n";
+            }
+            ClearList();
+            richTextBox.Text = output;
+
+            var min = gpa.Min();
+            var max = gpa.Max();
+            var sum = gpa.Sum();
+            var average = gpa.Average();
+            var maxNumberName = gpa.IndexOf(gpa.Max());
+            var minNumberName = gpa.IndexOf(gpa.Min());
+
+            maxNameTextBox.Text = studentName[maxNumberName];
+            minNameTextBox.Text = studentName[minNumberName];
+            maxTextBox.Text = max.ToString();
+            minTextBox.Text = min.ToString();
+            totalTextBox.Text = sum.ToString();
+            avarageTextBox.Text = average.ToString();
+
+        }
+        private void ClearList()
+        {
+            idTextBox.Clear();
+            nameTextBox.Clear();
+            mobileTextBox.Clear();
+            ageTextBox.Clear();
+            addressTextBox.Clear();
+            gpaTextBox.Clear();
+        }
+    }
 }
 
